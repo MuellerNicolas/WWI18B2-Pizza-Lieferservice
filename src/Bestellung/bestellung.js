c5674a685b8be598578554832c96910b03d729bc
Leonie Sinn <leonie.sinn@outlook.de>
2019-10-28 10:59:58 +0100

    EventListener für Buttons korrigiert

--- src/Bestellung/bestellung.js
+++ src/Bestellung/bestellung.js
@@ -30,46 +30,33 @@ class Bestellung{
         this._pageDom = document.createElement("div");
         this._pageDom.innerHTML = html;
 
+        let buttonAddPizzaFromMenu = this._pageDom.querySelector("#addPizzaFromMenu");
+        buttonAddPizzaFromMenu.addEventListener("click", () => this._onAddPizzaFromMenuClicked());
+
         let buttonAddTopping = this._pageDom.querySelector("#addTopping");
         buttonAddTopping.addEventListener("click", () => this._onAddToppingClicked());
+
+        let buttonAddPizzaIndividual = this._pageDom.querySelector("#addPizzaIndividual");
+        buttonAddPizzaIndividual.addEventListener("click", () => this._onAddIndividualClicked());
+
+        let buttonOrder = this._pageDom.querySelector("#order");
+        buttonOrder.addEventListener("click", () =>
+            location.hash = "#/Lieferung/");
 
         this._app.setPageTitle("Bestellung aufgeben", {isSubPage: true});
         this._app.setPageCss(css);
         this._app.setPageContent(this._pageDom);
     }
 
+    _onAddPizzaFromMenuClicked(){
+        alert("Add Pizza from Menu!");
+    }
+
     _onAddToppingClicked() {
         alert("Add Topping!");
     }
 
-    klick () {
-        let pizzaMenuButton = document.getElementById("addPizzaFromMenu");
-        pizzaMenuButton.addEventListener("click", addPizzaFromMenu);
-
-        let toppingButton = document.querySelector("addTopping");
-        toppingButton.addEventListener("click", () => this.addTopping);
-
-        let pizzaIndividualButton = document.querySelector("addPizza");
-        pizzaIndividualButton.addEventListener("click", () => this.addPizza);
-
-        let bestellenButton = document.querySelector("order");
-        bestellenButton.addEventListener("click", /*Methode*/);
-
-    }
-
-    addPizzaFromMenu () {
-        //neue Pizzazeile hinzufügen
-        // let liste = document.getElementById("pizzaAuswaehlen");
-        // let liElement = document.getElementById("auswahlZeile");
-        // liste.appendChild(liElement);
-        alert ("Hello!");
-    }
-
-    addTopping(){
-        //neue Toppingzeile hinzufügen
-    }
-
-    addPizza(){
-        //neue Pizzazeile hinzufügen (selbstkreierte Pizza)
+    _onAddIndividualClicked() {
+        alert("Add individual Pizza!")
     }
 }
