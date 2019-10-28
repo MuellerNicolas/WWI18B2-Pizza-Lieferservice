62208b54552464ddd1c3dc64d97a40b7b958b88d
Leonie Sinn <leonie.sinn@outlook.de>
2019-10-28 09:49:45 +0100

    Wiederherstellung der in der Lösung vorgegeben Aufrufstruktur

--- src/Bestellung/bestellung.js
+++ src/Bestellung/bestellung.js
@@ -1,8 +1,45 @@
-
+/**
+ * Seite zum Aufgeben einer neuen Bestellung.
+ */
 class Bestellung{
+    /**
+     * Konstruktor
+     * @param {App} app Zentrale Instanz der App-Klasse
+     */
+    constructor (app) {
+        this._app = app;
+    }
 
-    constructor{
+    /**
+     * Seite anzeigen. Wird von der App-Klasse aufgerufen.
+     */
+    async show(matches) {
+        // Anzuzeigenden Seiteninhalt nachladen
+        let html = await fetch("bestellung/bestellung.html");
+        let css = await fetch("bestellung/bestellung.css");
 
+        if (html.ok && css.ok) {
+            html = await html.text();
+            css = await css.text();
+        } else {
+            console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+            return;
+        }
+
+        // Seite zur Anzeige bringen
+        this._pageDom = document.createElement("div");
+        this._pageDom.innerHTML = html;
+
+        let buttonAddTopping = this._pageDom.querySelector("#addTopping");
+        buttonAddTopping.addEventListener("click", () => this._onAddToppingClicked());
+
+        this._app.setPageTitle("Bestellung aufgeben", {isSubPage: true});
+        this._app.setPageCss(css);
+        this._app.setPageContent(this._pageDom);
+    }
+
+    _onAddToppingClicked() {
+        alert("Add Topping!");
     }
 
     klick () {
@@ -35,3 +72,4 @@ class Bestellung{
     addPizza(){
         //neue Pizzazeile hinzufügen (selbstkreierte Pizza)
     }
+}
